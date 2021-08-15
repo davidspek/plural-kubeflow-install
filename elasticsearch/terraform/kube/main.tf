7976094a7b18095ab184e8abf0f8bf216f523881
DavidSpek <vanderspek.david@gmail.com>
2021-08-15 13:29:18 +0200

    Remove elasticsearch

--- elasticsearch/terraform/kube/main.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_namespace" "elasticsearch" {
-  metadata {
-    name = var.namespace
-    labels = {
-      "app.kubernetes.io/managed-by" = "plural"
-      "istio-injection" = "enabled"
-    }
-  }
-}
