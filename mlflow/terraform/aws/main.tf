172c10448620e1a50e2947019086098551a1fede
DavidSpek <vanderspek.david@gmail.com>
2022-02-16 16:25:53 +0100

    cleanup

--- mlflow/terraform/aws/main.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_namespace" "mlflow" {
-  metadata {
-    name = var.namespace
-    labels = {
-      "app.kubernetes.io/managed-by" = "plural"
-      "app.plural.sh/name" = "mlflow"
-    }
-  }
-}
--- spark/terraform/aws/main.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_namespace" "spark" {
-  metadata {
-    name = var.namespace
-    labels = {
-      "app.kubernetes.io/managed-by" = "plural"
-      "app.plural.sh/name" = "spark"
-    }
-  }
-}
