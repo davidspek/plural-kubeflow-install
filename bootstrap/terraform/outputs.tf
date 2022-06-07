36c96c814010eb2905bb9103dc4dcf7b92534f68
DavidSpek <vanderspek.david@gmail.com>
2022-06-07 13:57:55 +0200

    fix bootstrap deploy
    Signed-off-by: DavidSpek <vanderspek.david@gmail.com>

--- bootstrap/terraform/outputs.tf
+++ bootstrap/terraform/outputs.tf
@@ -1,29 +1,5 @@
-output "cluster_oidc_issuer_url" {
-	value = module.aws-bootstrap.cluster_oidc_issuer_url
-	sensitive = true
-}
-
-
 output "cluster_private_subnet_ids" {
 	value = module.aws-bootstrap.cluster_private_subnet_ids
-	sensitive = true
-}
-
-
-output "worker_role_arn" {
-	value = module.aws-bootstrap.worker_role_arn
-	sensitive = true
-}
-
-
-output "cluster_private_subnets" {
-	value = module.aws-bootstrap.cluster_private_subnets
-	sensitive = true
-}
-
-
-output "cluster_public_subnet_ids" {
-	value = module.aws-bootstrap.cluster_public_subnet_ids
 	sensitive = true
 }
 
@@ -40,6 +16,30 @@ output "cluster_worker_private_subnet_ids" {
 }
 
 
+output "node_groups" {
+	value = module.aws-bootstrap.node_groups
+	sensitive = true
+}
+
+
+output "worker_role_arn" {
+	value = module.aws-bootstrap.worker_role_arn
+	sensitive = true
+}
+
+
+output "cluster_oidc_issuer_url" {
+	value = module.aws-bootstrap.cluster_oidc_issuer_url
+	sensitive = true
+}
+
+
+output "cluster_public_subnet_ids" {
+	value = module.aws-bootstrap.cluster_public_subnet_ids
+	sensitive = true
+}
+
+
 output "cluster_worker_private_subnets" {
 	value = module.aws-bootstrap.cluster_worker_private_subnets
 	sensitive = true
@@ -52,8 +52,8 @@ output "endpoint" {
 }
 
 
-output "node_groups" {
-	value = module.aws-bootstrap.node_groups
+output "cluster_private_subnets" {
+	value = module.aws-bootstrap.cluster_private_subnets
 	sensitive = true
 }
 
